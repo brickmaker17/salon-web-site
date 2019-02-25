3f0a25ade4302755946c2a1a017997c03dc9bc3e
Luke Barrett <gotluke@gmail.com>
2019-02-24 22:41:44 -0700

    Added React router and styled components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>ABOUT US</li>
+                <li>SERVICES</li>
+                <li>GIFT CARDS</li>
+                <li>LOCATIONS</li>
+                <li>MY ACCOUNT</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
